e2d21e68fa49fdd953e79a084f800a62964b5f0e
Matěj <xkonop03@vutbr.cz>
2022-04-23 20:50:33 +0200

    added dev descriptor and filter creation

--- sniffer.cpp
+++ sniffer.cpp
@@ -1,9 +1,12 @@
 //#include <bits/getopt_core.h>
 //#include <cstddef>
 #include <cstddef>
+#include <cstdio>
 #include <getopt.h>
 #include <iostream> 	            //parse args
 #include <ostream>
+#include <pcap.h>
+#include <pcap/pcap.h>
 #include <string>
 #include <cstring>
 #include <bitset> 	            //printing
@@ -15,6 +18,7 @@
 #include <netinet/ip6.h> 	    //ipv6
 #include <netinet/tcp.h>	    //tcp
 #include <netinet/ip_icmp.h>	//ipv4, icmp
+#include <sys/types.h>
 
 #define SUCCES  0
 #define ERROR   1
@@ -22,23 +26,13 @@
 //std namespace
 using namespace std;
 
-/*Global flags for options*/
-
-
-
-// struct option longopts[] = {
-//    { "all",     no_argument,       & do_all,     1   },
-//    { "file",    required_argument, NULL,         'f' },
-//    { "help",    no_argument,       & do_help,    1   },
-//    { "verbose", no_argument,       & do_verbose, 1   },
-//    { 0, 0, 0, 0 }
-// };
-
 void print_interfaces();
 
 void print_help(char* progname);
 
+string load_filtr(int udp, int tcp, int port, int arp, int icmp);
 
+pcap_t* set_pcap_handle(string dev, string filter);
 
 int main(int argc, char* argv[])
 {
@@ -50,6 +44,12 @@ int main(int argc, char* argv[])
     int arp_flag = 0;
     //icmp only mode 
     int icmp_flag = 0;
+    //interface specificator
+    string interface = "";
+    //port specificator, -1 when all
+    int port = -1;
+    //packet number specifier, 1 when default
+    int packet_num = 1;
 
     //options
     static struct option long_opt[] = 
@@ -66,12 +66,6 @@ int main(int argc, char* argv[])
     };
 
     static char short_opt[] = "i::p:tun:h";
-    //interface specificator
-    string interface = "";
-    //port specificator, -1 when all
-    int port = -1;
-    //packet number specifier, 1 when default
-    int packet_num = 1;
     //cmd line argument
     int cla;
     //index
@@ -79,25 +73,28 @@ int main(int argc, char* argv[])
     //cla loading loop
     while((cla = getopt_long(argc, argv, short_opt, long_opt, &optind)) != -1)
     {
-        cout << "Caught arg " << optind  << endl;
         char* tmp_optarg = NULL;
         switch (cla) 
         {
             //flag set
             case 0:
                 break;
-            case 'i':
+            case 'i': //inspired by https://stackoverflow.com/questions/1052746/getopt-does-not-parse-optional-arguments-to-parameters
+                //check if the option argument exits
                 if(!optarg && argv[optind] != NULL && '-' != argv[optind][0])
                 {   
+                    //if so, load it to the tmp var
                     tmp_optarg = argv[optind];                   
                 }
+                //check if load occured
                 if(tmp_optarg){
-                    cout << "OK: " << tmp_optarg << endl;
+                    //save the option argument int othe interface variable
+                    interface.append(tmp_optarg);
                 }
+                // -i was specified alone, special case
                 else 
                 {   
                     // -i without param -> print interfaces
-                    cout << "-i empty"<<endl;
                     print_interfaces();
                 }
                 break;
@@ -134,17 +131,157 @@ int main(int argc, char* argv[])
                 break;
         }   
     }
+    cout << udp_flag << icmp_flag << tcp_flag << arp_flag << interface <<endl;
 
-    //check conflict void print_help()on args
-    if(udp_flag && tcp_flag)
+    
+    
+    //check if iface was set correctly
+    if(interface.empty())
     {
-        cerr << "ERROR: Unvalid argument combination: [-t|--tcp] and [-u|--udp]";
-        exit(ERROR);
+        cerr << "ERROR: No interface [-i|--interface] was specified. Use -h for help" << endl;
     }
-    cout << udp_flag << icmp_flag << tcp_flag << arp_flag <<endl;
+
+
+    string filter = "";
+    cout << port <<endl;
+    filter.append(load_filtr(udp_flag, tcp_flag, port, arp_flag, icmp_flag));
+    cout << "FILTER: " << filter;
+    
+
+
+    pcap_t* dev_descriptor = set_pcap_handle(interface, filter);
+    
+    
+
+    cout << "DONE"<<endl;
     return 0;
+
+    
+
 }
 
+/**
+ * @brief generates formated pcap filter
+ * 
+ * @param udp  udp flag
+ * @param tcp tcp flag
+ * @param port port num (-1 -> all)
+ * @param arp arp flag
+ * @param icmp icmp flag
+ * @return string formated pcap filter string
+ */
+string load_filtr(int udp, int tcp, int port, int arp, int icmp)
+{
+    // //check conflict void print_help()on args
+    // // if(udp && tcp)
+    // // {
+    // //     cerr << "ERROR: Unvalid argument combination: [-t|--tcp] and [-u|--udp]";
+    // //     exit(ERROR);
+    // // }
+    string filter = "";
+    string port_s = to_string(port);
+    bool use_or = false;
+
+    //
+    if(port >= 0)
+    {
+        if(udp)
+        {
+            filter.append("(udp port " + port_s + ")");
+            use_or = true;
+        }
+        if(tcp)
+        {
+            if(use_or)
+            {
+                filter.append(" or ");
+            }
+            filter.append("(tcp port " + port_s + ")");
+            use_or = true;
+        }
+    }
+    else 
+    {
+        if(udp)
+        {
+            filter.append("udp");
+            use_or = true;
+        }
+        if(tcp)
+        {
+            if(use_or)
+            {
+                filter.append(" or ");
+            }
+            filter.append("tcp");
+            use_or = true;
+        }
+    }
+    if(!use_or && port >= 0)
+    {
+        filter.append("Icmp or icmp6 or arp or ((tcp or udp) and port " + port_s + ")");
+    }
+
+
+    if(icmp)
+    {   
+        if(use_or)
+        {
+            filter.append(" or ");
+        }
+        filter.append("icmp or icmp6");
+        use_or = true;
+    }
+    if(arp)
+    {
+        if(use_or)
+        {
+            filter.append(" or ");
+        }
+        filter.append("arp");
+    }
+    
+    return filter;
+}
+
+pcap_t* set_pcap_handle(string dev, string filter)
+{
+    //init necessary pcap vars
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    bpf_u_int32 src_ip, net_mask;
+    struct bpf_program bpf_p;
+
+    //get iface netmask and ip for filter
+    if(pcap_lookupnet(dev.c_str(), &src_ip, &net_mask, error_buffer) < 0)
+    {
+        cerr << "ERROR during address lookup, pcap: " << error_buffer << endl;
+        exit(ERROR);
+    }
+
+    //open device for capturing traffic   
+    pcap_t *dev_descriptor; 
+    if((dev_descriptor = pcap_open_live(dev.c_str(), BUFSIZ, 1, 0, error_buffer)) == NULL)
+    {
+        cerr << "ERROR during handle creating, pcap: " << error_buffer << endl;
+        exit(ERROR);
+    }
+
+    //compile a filter from string 
+    if(pcap_compile(dev_descriptor, &bpf_p, filter.c_str(), 0, net_mask) < 0 )
+    {
+        cerr << "ERROR during filter compilation, pcap: " << error_buffer << endl;
+        exit(ERROR);
+    }
+
+    //aply filter from the compile bpf_p filter
+    if(pcap_setfilter(dev_descriptor, &bpf_p) < 0)
+    {
+        cerr << "ERROR during filter aplying, pcap: " << error_buffer << endl;
+        exit(ERROR);
+    }
+    
+    return dev_descriptor;
+}
 
 /**
  * @brief Prints the default help message and terminates the program
@@ -172,7 +309,24 @@ void print_help(char* progname)
  */
 void print_interfaces()
 {
-    cout << "print_interfaces called" << endl;
+    //init err buffer
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    //init IF pointers
+    pcap_if_t *aviable_interfaces;
+    
+    //load interfaces and check for correct execution - loads linked list into aviable interfaces
+    if (pcap_findalldevs(&aviable_interfaces, error_buffer) < 0) {
+        cerr << "ERROR: Internal PCAP lib error when searching for interfraces";
+        exit(ERROR);
+    }
+
+    //print all interfaces - linked list looping
+    auto *tmp = aviable_interfaces;
+    while(tmp != NULL)
+    {
+        cout << tmp->name <<endl;
+        tmp = tmp->next;
+    }
     exit(SUCCES);
 }
 
@@ -181,7 +335,7 @@ void print_interfaces()
  * -- nejspise neobsahuje CRC checksum
  * pcap_next() - vrati jeden paket, lze nastavit timeout
  * timestampy - Z jako centralni cas
- * ntohs, ntohl . pro pravne vypisy hodnot
+ * ntohs, ntohl . pro spravne vypisy hodnot
  * pcap_geterr -> vrati last command error
  * inet_ntop -. vypisovani ipv4/6 adres 198.142.234, dava pole charu !!! pro ipv6 dat velky buffer at nedela bordel
  * 
